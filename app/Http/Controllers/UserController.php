4c7f804446a80ed5f94cdbf0aa56cbc292fc1474
Aravindan12 <aravind@sparkouttech.com>
2023-10-08 11:42:09 +0530

    usage of clone

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function cloneUser()
+    {
+        $user = User::first();
+        $clone =  clone $user;
+        $user->name = "arvind";
+        $user->save();
+        return ['user' => $user->name, "clone" => $clone->name];
+    }
+}
